70440dd880d8a541ea61aea8984a95cf864db193
Dean <40185661+DeanSatine@users.noreply.github.com>
2025-09-02 01:20:02 -0400

    Dragable fixe

--- Assets/Scripts/Dragable.cs
+++ Assets/Scripts/Dragable.cs
@@ -1,94 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 using static UnitAI;
 
 public class Draggable : MonoBehaviour
 {
-    private Vector3 mouseOffset;
     public bool isDragging = false;
-    private bool hasUnitTag;
-    private bool hasTileTag;
     private Vector3 oldPosition;
     private int friendlyUnitLayer = 9;
 
     private void OnMouseDown()
     {
-        mouseOffset = transform.position - GetMouseWorldPos();
-        isDragging = true;
-        oldPosition = transform.position;
+        if (!isDragging)
+        {
+            // Pick up the unit
+            isDragging = true;
+            oldPosition = transform.position;
+        }
+        else
+        {
+            // Drop the unit
+            isDragging = false;
+            SnapToClosestObject();
+        }
     }
 
-    private void OnMouseDrag()
+    private void Update()
     {
         if (isDragging)
         {
-            Vector3 currentMousePos = GetMouseWorldPos();
-            transform.position = new Vector3(currentMousePos.x + mouseOffset.x, transform.position.y, currentMousePos.z + mouseOffset.z);
+            Vector3 mousePos = GetMouseWorldPos();
+            transform.position = new Vector3(mousePos.x, transform.position.y, mousePos.z);
         }
-    }
-
-    private void OnMouseUp()
-    {
-        isDragging = false;
-
-        // Check for collision with other objects and snap if needed
-        SnapToClosestObject();
     }
 
     private void SnapToClosestObject()
-  
     {
         TraitManager.Instance.EvaluateTraits(GameManager.Instance.playerUnits);
 
-        Collider[] colliders = Physics.OverlapCapsule(transform.position - Vector3.up * 0.5f, transform.position + Vector3.up * 0.5f, 0.5f); // Adjust the height and radius as needed
-
+        Collider[] colliders = Physics.OverlapCapsule(
+            transform.position - Vector3.up * 0.5f,
+            transform.position + Vector3.up * 0.5f,
+            0.5f);
 
         Transform closestObject = null;
         float closestDistance = float.MaxValue;
-        hasTileTag = false;
-        hasUnitTag = false;
 
         foreach (var collider in colliders)
         {
-            if (collider.gameObject != gameObject)// Exclude self
+            if (collider.gameObject == gameObject) continue;
+
+            float distance = Vector3.Distance(transform.position, collider.transform.position);
+            if (distance < closestDistance)
             {
-                //Change this to check friendly unit Note: implementation below did not fix issue.
-                //if (collider.CompareTag("Unit"))
-                if (collider.gameObject.layer.Equals(friendlyUnitLayer))
-                {
-                    hasUnitTag = true;
-                }
-                if (collider.CompareTag("Tile"))
-                {
-                    hasTileTag = true;
-                }
-
+                closestObject = collider.transform;
+                closestDistance = distance;
             }
-        }
-        foreach (var collider in colliders) 
-        { 
-
-            if ((!(hasTileTag && hasUnitTag)) || (!hasUnitTag))
-            {
-                if (collider.gameObject != gameObject) // Exclude self
-                {
-                    float distance = Vector3.Distance(transform.position, collider.transform.position);
-
-                    if (distance < closestDistance)
-                    {
-                        closestObject = collider.transform;
-                        closestDistance = distance;
-                    }
-                }
-            }
-            
         }
 
         if (closestObject != null)
         {
-            transform.position = new Vector3(closestObject.position.x, transform.position.y, closestObject.position.z);
+            transform.position = new Vector3(
+                closestObject.position.x,
+                transform.position.y,
+                closestObject.position.z);
 
             var unitAI = GetComponent<UnitAI>();
             var tile = closestObject.GetComponent<HexTile>();
@@ -113,8 +88,6 @@ public class Draggable : MonoBehaviour
         {
             transform.position = oldPosition;
         }
-
-
     }
 
     private Vector3 GetMouseWorldPos()
@@ -127,6 +100,6 @@ public class Draggable : MonoBehaviour
             return hit.point;
         }
 
-        return Vector3.zero; // Return a default value if no hit point is found.
+        return Vector3.zero;
     }
 }
