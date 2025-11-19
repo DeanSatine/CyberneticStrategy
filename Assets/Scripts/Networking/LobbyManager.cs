0b88eb0f330d51c00e7bd3d92dec5f2941ebc2ca
Dean <herod3n9@gmail.com>
2025-11-19 14:45:37 -0500

    Reapply "Menu & Lobby Networking"
    This reverts commit cb820f3b29a54c4b5c745c4471af9d55416b3067.

--- Assets/Scripts/Networking/LobbyManager.cs
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -103,7 +103,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         PhotonNetwork.CurrentRoom.IsOpen = false;
         PhotonNetwork.CurrentRoom.IsVisible = false;
 
-        PhotonNetwork.LoadLevel("Game");
+        PhotonNetwork.LoadLevel("Test");
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
